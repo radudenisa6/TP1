19649c10e3bed15d713bec11a3a43378a858ad45
radudenisa6 <61680499+radudenisa6@users.noreply.github.com>
2020-03-31 18:35:11 +0300

    Add files via upload

---
+++ L5/B1.c
@@ -0,0 +1,31 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 16
+VisualStudioVersion = 16.0.29326.143
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "tema5", "tema5\tema5.vcxproj", "{E01131A1-B8F2-465C-9226-C66B48394BFB}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|x64 = Debug|x64
+		Debug|x86 = Debug|x86
+		Release|x64 = Release|x64
+		Release|x86 = Release|x86
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{E01131A1-B8F2-465C-9226-C66B48394BFB}.Debug|x64.ActiveCfg = Debug|x64
+		{E01131A1-B8F2-465C-9226-C66B48394BFB}.Debug|x64.Build.0 = Debug|x64
+		{E01131A1-B8F2-465C-9226-C66B48394BFB}.Debug|x86.ActiveCfg = Debug|Win32
+		{E01131A1-B8F2-465C-9226-C66B48394BFB}.Debug|x86.Build.0 = Debug|Win32
+		{E01131A1-B8F2-465C-9226-C66B48394BFB}.Release|x64.ActiveCfg = Release|x64
+		{E01131A1-B8F2-465C-9226-C66B48394BFB}.Release|x64.Build.0 = Release|x64
+		{E01131A1-B8F2-465C-9226-C66B48394BFB}.Release|x86.ActiveCfg = Release|Win32
+		{E01131A1-B8F2-465C-9226-C66B48394BFB}.Release|x86.Build.0 = Release|Win32
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {32EF7A30-2378-4F7C-B98E-76F56439B486}
+	EndGlobalSection
+EndGlobal
